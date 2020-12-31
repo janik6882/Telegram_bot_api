fdd41480e22643318712c8aee530cecc51329922
janik6882 <janik@klauenberg.eu>
2020-12-31 17:11:09 +0100

    added send_poll(), not yet working

--- Bot.py
+++ Bot.py
@@ -108,15 +108,17 @@ class Wrapper():
         return json.loads(r.content)
 
     # TODO: Add send_audio, send_document, send_video, send_animation, send_voice, send_video_note and send_media_group
-    def send_location(self, long, lat, horizontalAccuracy=None, disableNotifiation=False):
+    def send_location(self, chatId, long, lat, horizontalAccuracy=None, disableNotifiation=False, livePeriod=None, heading=None):
         """
         Comment: send a location by it's longitude and latitude to a chatId
         Input: Name of Instance, longitude, latitude, optional: accuracy and disableNotifiation
         Output: Server Response as Json Object
         Special: Nothing special
         """
+        # TODO: Live Location not working, fix later.
         url = self.base + "/sendLocation"
         params = {
+                  "chat_id": chatId,
                   "longitude": long,
                   "latitude": lat,
                   "horizontal_accuracy": horizontalAccuracy,
@@ -124,7 +126,42 @@ class Wrapper():
         }
         r = requests.get(url, params=params)
         return json.loads(r.content)
-
+     # TODO: Add edit and stop Live location
+    def send_venue(self, chatId, long, lat, title, adress):
+        """
+        Comment: Send a venue to a user
+        Input: # TODO: finish docu
+        Output:
+        Special:
+        """
+        url = self.base + "/sendVenue"
+        params = {
+                  "chat_id": chatId,
+                  "longitude": long,
+                  "latitude": lat,
+                  "title": title,
+                  "adress": adress,
+        }
+        r = requests.get(url, params=params)
+        return json.loads(r.content)
+    # TODO: Add send_contact
+    def send_poll(self, chatId, question, options, correctOption=None, type=None):
+        """
+        Comment: # TODO: Add docu
+        Input:
+        Output:
+        Special:
+        """
+        url = self.base + "/sendPoll"
+        params = {
+                  "chat_id": chatId,
+                  "question": question,
+                  "options": options,
+                  "correct_option": correctOption,
+                  "type": type,
+        }
+        r = requests.get(url, params=params)
+        return json.loads(r.content)
 
 
 def main():
@@ -132,7 +169,7 @@ def main():
     token = data["token"]
     myid = data["my_id"]
     test = Wrapper(token)
-    x = test.send_photo(myid, "test.jpg", "Dies ist ein Test", "True")
+    x = test.send_poll(myid, "Wer ist der groesste?", list(["Janik", "Ich", "Der da"]))
     # for i in x["result"]:
     print (x)
 
